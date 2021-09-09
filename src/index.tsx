eaec77ff386d7f9a253e373da4498b0e2fea04da
Dowglas Maia <dowglasmaia@gmail.com>
2021-09-08 22:07:04 -0300

    Correção de declarção sem de Compnente,sem Uso

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/style.css';
 
 import App from './App';
-import NavBar from 'components/NavBar';
 
 ReactDOM.render(
   <React.StrictMode>    
