076afdc9faf71bccdb47c0932a3b3e5208296754
dowglasmaia <dowglasmaia@gmail.com>
2021-10-13 22:54:35 -0300

    fazendo um map dos dados de response da API

--- src/components/DonutCharts/index.tsx
+++ src/components/DonutCharts/index.tsx
@@ -1,20 +1,48 @@
+import axios from "axios";
 import Chart from "react-apexcharts";
+import { SaleSum } from "types/sale";
+import { BASE_URL } from "utils/requests";
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+}
 
 const DonutChart = () => {
-  const mockData = {
+
+  /*criando uma varial recebendo otipo ChartData */
+  let chartData: ChartData = {
+    labels: [],
+    series: []
+  };
+
+  axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    .then((response) => {
+      const data = response.data as SaleSum[]; // fazendo CAST do response da API para meu tipo de objeto
+      const myLabels = data.map(sale => sale.sellerName);
+      const mySeries = data.map(sale => sale.sum);
+
+      chartData = { labels: myLabels, series: mySeries }
+
+      console.log(response.data)
+      console.log(chartData)
+    });
+
+
+  /*const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
     labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
   };
+  */
 
   const options = {
-    legend: {
-      show: true,
-    },
+    legend: { show: true },
   };
+
   return (
     <Chart
-      options={{ ...options, labels: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
       type="donut"
       height="240"
     />
